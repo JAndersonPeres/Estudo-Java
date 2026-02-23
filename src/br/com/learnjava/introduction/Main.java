0e2005bc50e4c62779aad7c3bc57baa875a4b075
Anderson Peres <jpdandersonalencar@gmail.com>
2026-02-23 20:40:26 -0300

    Class 003 - Comments

--- src/br/com/learnjava/introduction/Main.java
+++ src/br/com/learnjava/introduction/Main.java
@@ -1,6 +1,13 @@
 package br.com.learnjava.introduction;
-
+/**
+ * This is a <b>javadoc</b> comment.
+ */
 public class Main{
+    // This is a One Line Comment
+    /*
+        This is a Multiple
+        Line Comment
+     */
     public static void main(String[] args){
         System.out.println("KA-ME-HA-ME-HAAA");
     }
